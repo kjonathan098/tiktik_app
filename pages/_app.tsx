451de661f0547d30cd19ad8afde577c65d37e57b
kjonathan098 <kelman.jonathan@gmail.com>
2022-07-12 22:18:29 +0300

    sideBar login btn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {useState, useEffect} from 'react'
+import Sidebar from '../components/Sidebar'
+import NavBar from '../components/Navbar'
 
 const MyApp = ({Component, pageProps}: AppProps) => {
 	const [isSSR, setisSSR] = useState(true)
@@ -11,12 +13,14 @@ const MyApp = ({Component, pageProps}: AppProps) => {
 	if (isSSR) return null
 	return (
 		<div>
-			NavBar
+			<NavBar />
 			<div className="flex gap-6 md:gap-20">
-				<div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">sideBar</div>
-			</div>
-			<div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
-				<Component {...pageProps} />
+				<div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+					<Sidebar />
+				</div>
+				<div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+					<Component {...pageProps} />
+				</div>
 			</div>
 		</div>
 	)
